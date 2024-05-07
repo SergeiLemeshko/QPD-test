a0c01c860a042b8f643221600dba5fae4fc13e34
Sergey Lemeshko <lemexaxa@gmail.com>
2024-05-07 13:58:35 +0300

    refactor: redesigned the methods promiseAll and promiseRace

--- Task_3/promise.js
+++ Task_3/promise.js
@@ -14,57 +14,50 @@ delay(2000).then(
 
 //---------------------- promiseAll ----------------------------
 
-// URLs трех изображений с JSONPlaceholder
-const urlsImg = [
-  'https://jsonplaceholder.typicode.com/photos/1',
-  'https://jsonplaceholder.typicode.com/photos/2',
-  'https://jsonplaceholder.typicode.com/photos/3'
+const promises = [
+  promiseOne = new Promise((resolve, reject) => {setTimeout(() => {resolve("Resolved-1")}, 1000)}),
+  promiseTwo = new Promise((resolve, reject) => {setTimeout(() => {resolve("Resolved-2")}, 500)}),
+  promiseThree = new Promise((resolve, reject) => {resolve("Resolved-3")})
 ];
 
-// Добавляет изображения на страницу
-function loadImage(url) {
+function promiseAll(promises) {
+  if(!promises.length) return;
+  let result = new Array(promises.length); // массив длиной, как и массив promises
+  let totalResolved = 0;
+
   return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = () => reject(new Error(`Не удалось загрузить изображение по URL: ${url}`));
-    img.src = url;
-    document.body.appendChild(img);
-    img.classList.add('image');
+    promises.forEach((promise, index) => {
+      promise
+      .then((val) => {
+        result[index] = val; // сохраняем результат promise с тем же индексом
+        totalResolved++;
+        if (totalResolved === promises.length) {
+          resolve(result); // если все promises выполнены, показываем результат
+          console.log(result, "promiseAll");
+        }
+      }).catch((err) => {
+        reject(err); // если какой-либо promise отклонён, вызываем reject
+        console.log(err);
+      });
+    });
   });
-}
+};
 
-// Дожидаемся выполнения всех промисов с использованием Promise.all
-function promiseAll(urls) {
-  return Promise.all(urls.map(url => fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Ошибка загрузки изображения: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => data.url)
-    .then(imageUrl => loadImage(imageUrl)) // Добавляем на страницу
-  ))
-  .catch(error => {
-    console.error('Ошибка загрузки изображений:', error);
-  });
-}
-
-promiseAll(urlsImg);
+promiseAll(promises);
 
 //---------------------- promiseRace ----------------------------
 
-// массив промисов
-const promises = [
-  promiseOne = new Promise((resolve) => { setTimeout(resolve, 1000, "Hello") }),
-  promiseTwo = new Promise((resolve) => { setTimeout(resolve, 2000, "World") })
-];
-
-// Принимает несколько промисов и возвращает первый завершенный промис
 function promiseRace(promises) {
-  Promise.race(promises)
-      .then(value => console.log(value))
-      .catch(error => console.log(error));
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      promise
+        .then(resolve) // как только будет выполнен какой-либо из входных promises
+        .catch(reject); // как только будет отклонен какой-либо из входных promises
+    });
+  });
 }
 
-promiseRace(promises);
+promiseRace(promises)
+  .then(res => console.log(res, "promiseRace"))
+  .catch(err => console.log(err));
+
