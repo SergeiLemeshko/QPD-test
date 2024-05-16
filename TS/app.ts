7e627e830b23836158113d18845cef5d31e8fd4c
Sergey Lemeshko <lemexaxa@gmail.com>
2024-05-16 11:57:05 +0300

    wip: add type Readonly

--- TS/app.ts
+++ TS/app.ts
@@ -39,5 +39,34 @@ const f = (value: Human) => {
   console.log(`Я ${value.name}, мне ${value.age}. Мой телефон: ${value.phoneNumber}`);
 }
 
-f(user);
-f(admin);
+// f(user);
+// f(admin);
+
+//------------------- Readonly ---------------------//
+
+type MyReadonly<T> = {
+  readonly [P in keyof T]: MyReadonly<T[P]>;
+};
+
+interface someUser {
+  name: string;
+  age: number;
+  address: {
+    city: string;
+    country: string;
+  };
+}
+
+type ReadonlySomeUser = MyReadonly<someUser>;
+
+const someUser: ReadonlySomeUser = {
+  name: "Alex",
+  age: 30,
+  address: {
+    city: "Voronezh",
+    country: "Russia",
+  },
+};
+
+someUser.name = "Bob";
+someUser.address.city = "Moscow";
